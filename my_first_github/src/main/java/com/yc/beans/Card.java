b1d20ebc7ef1c1051761e182ca933ffba99b62e1
Administrator <Administrator@SZLG0I3TRVNK0WY>
2018-09-08 08:56:08 +0800

    新增Card类

---
+++ my_first_github/src/main/java/com/yc/beans/Card.java
@@ -0,0 +1,27 @@
+package com.yc.beans;
+
+public class Card {
+
+	private int cId;
+	private int cType;
+	private int uid;
+	public int getcId() {
+		return cId;
+	}
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+	public int getcType() {
+		return cType;
+	}
+	public void setcType(int cType) {
+		this.cType = cType;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+}
