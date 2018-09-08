cc90bb61b215dd2547ce292c1b36fc4e7e3b8e20
Administrator <Administrator@SZLG0I3TRVNK0WY>
2018-09-08 08:46:27 +0800

    新增用户实体类

---
+++ my_first_github/src/main/java/com/yc/beans/User.java
@@ -0,0 +1,36 @@
+package com.yc.beans;
+
+public class User {
+
+	private int uId;
+	private String uName;
+	private int uSex;
+	private String uAddr;
+	
+	public int getuId() {
+		return uId;
+	}
+	public void setuId(int uId) {
+		this.uId = uId;
+	}
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public int getuSex() {
+		return uSex;
+	}
+	public void setuSex(int uSex) {
+		this.uSex = uSex;
+	}
+	public String getuAddr() {
+		return uAddr;
+	}
+	public void setuAddr(String uAddr) {
+		this.uAddr = uAddr;
+	}
+	
+	
+}
