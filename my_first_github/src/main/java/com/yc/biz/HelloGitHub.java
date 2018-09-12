c4e3eadc7dca49672b994aa9531eb909036045ce
Administrator <Administrator@SZLG0I3TRVNK0WY>
2018-09-12 08:54:02 +0800

    哈哈

--- my_first_github/src/main/java/com/yc/biz/HelloGitHub.java
+++ my_first_github/src/main/java/com/yc/biz/HelloGitHub.java
@@ -4,7 +4,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git_Hub !!!");
-		System.out.println("Hello Git_Hub !!!");
+		System.out.println("哈哈");
 		System.out.println("123");
 	}
 }
