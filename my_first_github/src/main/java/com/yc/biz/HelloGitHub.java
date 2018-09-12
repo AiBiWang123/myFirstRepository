b62747f370b30a7f746ad60f28c2bf9e78559d49
Administrator <Administrator@SZLG0I3TRVNK0WY>
2018-09-12 09:10:54 +0800

    dsad

--- my_first_github/src/main/java/com/yc/biz/HelloGitHub.java
+++ my_first_github/src/main/java/com/yc/biz/HelloGitHub.java
@@ -3,7 +3,6 @@ package com.yc.biz;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("========");
 		System.out.println("你想干嘛 !!!");
 		System.out.println("哈哈");
 		System.out.println("嘻嘻");
