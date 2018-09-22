b3ee8003996970bf95663392f76fe1c2b4b730c9
duanbihong <Administrator@SKY-20180816AGJ>
2018-09-22 17:21:51 +0800

    小红修改的Hello

--- my_first_github/src/main/java/com/yc/biz/HelloGitHub.java
+++ my_first_github/src/main/java/com/yc/biz/HelloGitHub.java
@@ -3,10 +3,10 @@ package com.yc.biz;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("===噢噢噢噢噢噢噢噢噢噢噢噢=====");
-		System.out.println("!!!");
+		System.out.println("===这行是小红修改的代码1=====");
+		System.out.println("===这行是小红修改的代码2=====");
 		System.out.println("哈哦");
-		System.out.println("123");
+		System.out.println("===这行是小红修改的代码3=====");
 		System.out.println("哈哈");
 		System.out.println("嘻嘻");
 		System.out.println("hh");
